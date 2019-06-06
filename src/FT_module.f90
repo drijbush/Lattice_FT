d7e7ea16ed9fc6f95d76e61c978fa14965b51e39
Ian Bush <Ian.Bush@oerc.ox.ac.uk>
2019-06-06 11:42:05 +0100

    Start of new lattice FT

---
+++ src/FT_module.f90
@@ -0,0 +1,10 @@
+Module MPP_Fourier_transform
+
+  Implicit None
+
+Contains
+
+  Subroutine MPP_Lattice_FT( xg_irr, n_spin, k_point_type, k_points
+  End Subroutine MPP_Lattice_FT
+  
+End Module MPP_Fourier_transform
